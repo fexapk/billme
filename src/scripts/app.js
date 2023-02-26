52dd43a3a2d78048ede428a42fb299cc2702e8f9
Spike <acsiidax@protonmail.com>
2023-02-26 16:35:18 +0100

    added function start which runs at page start up, where clean button funtionality and events are added

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,6 +1,57 @@
 (function main() {
-    const orderTemplate = document.getElementsByClassName('order');
-    const orderWrapper = document.getElementById('order-list-wrapper');
-    
-    
+    start();
+
+    function start() {
+
+        const cleanButton = document.querySelector('#clean-button');
+        const mainWrapper = document.querySelector('#main-wrapper');
+
+        mainWrapper.style.visibility = 'hidden';
+
+        cleanButton.addEventListener('click', () => {
+
+            if (cleanButton.innerText === 'create') {
+                cleanButton.innerText = 'clean';
+                mainWrapper.style.visibility = 'visible';
+            } else {
+                cleanButton.innerText = 'create';
+                mainWrapper.style.visibility = 'hidden';
+            }
+        });
+
+        function addOrder() {
+             
+        }
+    }
+
+    const orderHtml = 
+        `<div class="order">
+        <div>
+            <button class="add-button">+</button>
+        </div>
+        <div>
+            <input type="text" name="description">
+        </div>
+        <div>
+            <input type="number" name="unit">
+        </div>
+        <div>
+            <input type="number" name="unit-price">
+        </div>
+        <div>
+            <select name="iva">
+                <option value="4%">4%</option>
+                <option value="10%">10%</option>
+                <option value="21%">21%</option>
+            </select>
+        </div>
+        <div>
+            <output>
+                <span>&euro;</span>
+            </output>
+        </div>
+        <div>
+            <button class="delete-button">delete</button>
+        </div>
+    </div>`;
 })();
