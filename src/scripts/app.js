87be98ba757c5113adedd5e79b9d06c16d4cbcdd
fexport <jefax@tutamail.com>
2023-02-27 10:51:06 +0100

    finished main functionalities, it works as intended

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -5,6 +5,7 @@
 
         const cleanButton = document.querySelector('#clean-button');
         const mainWrapper = document.querySelector('#main-wrapper');
+        const orderListWrapper = document.querySelector('#order-list-wrapper');
 
         mainWrapper.style.visibility = 'hidden';
 
@@ -13,18 +14,66 @@
             if (cleanButton.innerText === 'create') {
                 cleanButton.innerText = 'clean';
                 mainWrapper.style.visibility = 'visible';
+                addOrder();
             } else {
                 cleanButton.innerText = 'create';
                 mainWrapper.style.visibility = 'hidden';
+                orderListWrapper.textContent = '';
             }
         });
-
-        function addOrder() {
-             
-        }
     }
 
-    const orderHtml = 
+    function addOrder() {
+        const newOrder = document.createElement('div');
+        const listOrderWrapper = document.querySelector('#order-list-wrapper');
+
+        newOrder.classList.add('order');
+        newOrder.insertAdjacentHTML('afterbegin', innerOrderTemplate);
+
+        listOrderWrapper.appendChild(newOrder);
+
+        const unitInput = newOrder.querySelector('input[name="unit"]');
+        const priceInput = newOrder.querySelector('input[name="unit-price"]');
+        const orderSelect = newOrder.querySelector('select');
+
+        unitInput.addEventListener('input', () => showOuput(unitInput, priceInput));
+        priceInput.addEventListener('input', () => showOuput(priceInput, unitInput));
+        orderSelect.addEventListener('change', () => showOuput(priceInput, unitInput));
+
+        addButtonListeners();
+
+        function showOuput(changedInput, unchangedInput) {
+
+           const orderOutput = newOrder.querySelector('output');
+
+           if (Number(changedInput.value) < 0) 
+               alert(changedInput.name + " can not be negative");
+           else if (unchangedInput == null)
+               alert(unchangedInput.name + " have to be filled first");
+           else {
+               const grossPrice = Number(changedInput.value) * Number(unchangedInput.value);
+               const iva = Number(orderSelect.value) * grossPrice;
+               const total = grossPrice + iva;
+               orderOutput.innerText = total;
+           }
+        }
+
+        function addButtonListeners() {
+            const addButton = newOrder.querySelector('.add-button');
+            const deleteButton = newOrder.querySelector('.delete-button');
+
+            deleteButton.addEventListener('click', () => {
+                newOrder.textContent = '';
+                newOrder.remove();
+            });
+
+            addButton.addEventListener('click', () => {
+                addOrder();
+            });
+        }
+   }
+
+    const innerOrderTemplate = 
         `<div>
             <button class="add-button">+</button>
         </div>
@@ -39,15 +88,13 @@
         </div>
         <div>
             <select name="iva">
-                <option value="4%">4%</option>
-                <option value="10%">10%</option>
-                <option value="21%">21%</option>
+                <option value="0.04">4%</option>
+                <option value="0.1">10%</option>
+                <option value="0.21">21%</option>
             </select>
         </div>
         <div>
-            <output>
-                <span>&euro;</span>
-            </output>
+            <output></output>
         </div>
         <div>
             <button class="delete-button">delete</button>
